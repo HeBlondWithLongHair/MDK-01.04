473d5c4a0e88c74e2d190588067cfb785c277eaf
HeBlondWithLongHair <advol231231231@gmail.com>
2023-02-13 23:10:37 +0300

    Class Phone

--- Class Phone Ande Person/src/Phone.java
+++ Class Phone Ande Person/src/Phone.java
@@ -1,5 +1,5 @@
 public class Phone
-{ 
+{
     public static void main(String[] args)
     {
         Phone phone1 = new Phone(" 89042597945 ","R2D2",80.2f);
@@ -65,4 +65,3 @@ public class Phone
     }
 
 }
-
